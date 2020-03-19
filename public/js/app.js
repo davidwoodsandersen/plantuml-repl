9e32c5d4f7e346ff2ef13aea33c5a7e543fb03bb
David Andersen <davidwoodsandersen@gmail.com>
2020-03-19 15:42:10 -0400

    Refactor to support larger diagrams

--- public/js/app.js
+++ public/js/app.js
@@ -3,27 +3,28 @@ var editor;
 var output;
 var buttons;
 
-// Utility method for making HTTP GET requests:
-function request(url, callback) {
+// Utility method for making HTTP POST requests:
+function request(url, body, callback) {
   var req = new XMLHttpRequest();
   req.addEventListener('load', callback);
-  req.open('GET', url);
-  req.send();
+  req.open('POST', url);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(JSON.stringify(body));
 }
 
 // Insert the result of the render request
 // into the output div:
 function tryRender() {
   setOutput('<p>Rendering...</p>');
-  var endpoint = '/render/svg?uml=' + encodeURIComponent(editor.value);
-  request(endpoint, function() {
+  var endpoint = '/render/svg';
+  request(endpoint, { uml: encodeURIComponent(editor.value) }, function() {
     setOutput(this.responseText);
   });
 }
 
-// Utility method for setting output HTML:
-function setOutput(html) {
-  output.innerHTML = html;
+// Utility method for setting output:
+function setOutput(data) {
+  output.innerHTML = data;
 }
 
 // Initializes the text editor by adding
@@ -37,32 +38,20 @@ function initOutput() {
   return document.querySelector('#output div');
 }
 
-// Initializes button elements:
-function initButtons() {
-  var render = document.querySelector('#render');
-  var getSvg = document.querySelector('#svg');
-  var getPng = document.querySelector('#png');
+// Initializes button element:
+function initButton() {
+  var render = document.getElementById('render');
 
   render.addEventListener('click', tryRender);
-  getSvg.addEventListener('click', function() {
-    window.open('/render/svg?uml=' + encodeURIComponent(editor.value));
-  });
-  getPng.addEventListener('click', function() {
-    window.open('/render/png?uml=' + encodeURIComponent(editor.value));
-  });
 
-  return {
-    render: render,
-    getSvg: getSvg,
-    getPng: getPng
-  };
+  return render;
 }
 
 // Application entrypoint:
 function main() {
   editor = initEditor();
   output = initOutput();
-  buttons = initButtons();
+  button = initButton();
 }
 
 main();
